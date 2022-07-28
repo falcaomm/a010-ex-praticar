70113c1c300c94d934ab421761b06fe5d2352d98
falcaomm <manu.m.falcao@gmail.com>
2022-07-28 18:25:59 -0300

    Exercício 3

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,6 +1,6 @@
 let array1 = [5,9,3]
 let array2 = ["a","e","i","o","u"]
-let array3 = [13, "março", 2>1]
+let array3 = [13,"outubro",true]
 
 console.log(array1);
 console.log(array2);
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,6 +1,6 @@
 let array1 = [5,9,3]
 let array2 = ["a","e","i","o","u"]
-let array3 = [13, "março",  2>1]
+let array3 = [13,"outubro",true]
 
 console.log(`array1: ${array1}`);
 console.log(`array2: ${array2}`);
@@ -15,4 +15,4 @@ console.log(`segundo item array2: ${array2[1]}`);
 console.log(`terceiro item array3: ${array3[2]}`);
 
 console.log(`array1 tem ${Number(5)}: ${array1.includes(5)}`);
-console.log(`array3 tem ${3>4}: ${array3.includes(false)}`);
+console.log(`array3 tem ${1>3}: ${array3.includes(false)}`);
