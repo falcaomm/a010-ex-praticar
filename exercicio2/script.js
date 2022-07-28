963399e7213c7d5fe67c87569185c2f7aa815e0f
falcaomm <manu.m.falcao@gmail.com>
2022-07-28 15:06:29 -0300

    Exercício 2

---
+++ exercicio2/script.js
@@ -0,0 +1,18 @@
+let array1 = [5,9,3]
+let array2 = ["a","e","i","o","u"]
+let array3 = [13, "março",  2>1]
+
+console.log(`array1: ${array1}`);
+console.log(`array2: ${array2}`);
+console.log(`array3: ${array3}`);
+
+console.log(`array1 tamanho: ${array1.length}`);
+console.log(`array2 tamanho: ${array2.length}`);
+console.log(`array3 tamanho: ${array3.length}`);
+
+console.log(`primeiro item array1: ${array1[0]}`);
+console.log(`segundo item array2: ${array2[1]}`);
+console.log(`terceiro item array3: ${array3[2]}`);
+
+console.log(`array1 tem ${Number(5)}: ${array1.includes(5)}`);
+console.log(`array3 tem ${3>4}: ${array3.includes(false)}`);
